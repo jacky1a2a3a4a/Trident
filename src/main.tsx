8db2f9b039f162cd576746545ab839cef3df44ac
jacky1a2a3a4a <jacky1a2a3a4a@gmail.com>
2025-06-16 18:00:00 +0800

    Update package.json to add homepage and deployment scripts. Modify pnpm-lock.yaml to include gh-pages dependency. Refactor vite.config.ts to set base path for deployment and adjust path resolution. Simplify imports in main.tsx for cleaner code.

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'src/styles/Theme.ts';
-import GlobalStyle from 'src/styles/GlobalStyle.ts';
-import App from 'src/App.tsx';
+import GlobalStyle from 'src/styles/GlobalStyle';
+import App from 'src/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: '/trident',
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
-      src: resolve(__dirname, './src'),
+      src: path.resolve(__dirname, './src'),
     },
   },
 });
